6634b37a8b4a21b1978c12673531bb807f79f7d1
dheera-01-dev <dheeraj.01.dev@gmail.com>
2024-10-31 10:18:24 +0530

    folder setup

---
+++ application/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Welcome to Edge Of War
+    </div>
+  )
+}
+
+export default page
---
+++ server/src/app.ts
@@ -0,0 +1,40 @@
+import { config } from 'dotenv';
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
+
+config();
+import cors from 'cors'
+
+const app = express();
+app.use(cors())
+
+app.use(express.json({
+  limit: 5000000
+}), (error:any, req:Request, res:Response, next:NextFunction)=>{
+  if (error) {
+    return res.status(400).json({ 
+      success: false,
+      error
+    });
+  }
+  next();
+});
+
+app.all('*', (req, res)=>{
+  res.status(404).json({
+    success: false,
+    message: "route not found!"
+  })
+});
+
+app.use((err:any, req:any, res:any, next:any)=>{
+  if(err){
+    return res.status(500).json({
+      success: false,
+      message: "internal server error!"
+    })
+  }
+});
+
+
+
+export default app;
